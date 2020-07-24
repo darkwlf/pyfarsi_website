484385aee2096fb31366c366a7644cb336b39841
Rez <rezfd99@gmail.com>
2020-07-24 20:37:14 +0430

    fixed last merge

--- account/urls.py
+++ account/urls.py
@@ -5,8 +5,8 @@ urlpatterns = (
     path('login/', views.Login.as_view(), nane='login'),
     path('register/', views.Register.as_view(), name='register'),
     path('reset/', views.UserPassReset.as_view(), name='reset_pass'),
-    path('reset/done/', views.PasswordResetDone.as_view(), name='password_reset_done'),
+    path('reset-done/', views.PasswordResetDone.as_view(), name='password_reset_done'),
     path('confirm/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
-    path('confirm/done/', views.PasswordResetComplete.as_view(), name='password_reset_complete'),
+    path('confirm-done/', views.PasswordResetComplete.as_view(), name='password_reset_complete'),
     path('logout/', views.Logout.as_view(), name='logout')
 )

1dd6dbeb81165cf93e13bec91623606ac5b85da6
parsa rahbari <53294224+parsa-rahbari-82@users.noreply.github.com>
2020-07-24 17:01:56 +0430

    'reset_password'

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,9 @@ from . import views
 app_name = 'account'
 urlpatterns = (
     path('login/', views.Login.as_view(), nane='login'),
-    path('register/', views.Register.as_view(), name='register')
+    path('register/', views.Register.as_view(), name='register'),
+    path('reset/', views.UserPassReset.as_view(), name='reset_pass'),
+    path('reset/done/', views.PasswordResetDone.as_view(), name='password_reset_done'),
+    path('confirm/<uidb64>/<token>/', views.PasswordResetConfirm.as_view(), name='password_reset_confirm'),
+    path('confirm/done/', views.PasswordResetComplete.as_view(), name='password_reset_complete'),
 )
--- account/views.py
+++ account/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.contrib.auth import views as auth_views
+from django.urls import reverse_lazy
 
-# Create your views here.
+
+class UserPassReset(auth_views.PasswordResetView):
+    template_name = 'accounts/password_reset_form.html'
+    success_url = reverse_lazy('accounts:password_reset_done')
+    email_template_name = 'accounts/password_reset_email.html'
+
+
+class PasswordResetDone(auth_views.PasswordResetDoneView):
+    template_name = 'accounts/reset_done.html'
+
+
+class PasswordResetConfirm(auth_views.PasswordResetConfirmView):
+    template_name = 'accounts/password_reset_confirm.html'
+    success_url = reverse_lazy('accounts:password_reset_complete')
+
+
+class PasswordResetComplete(auth_views.PasswordResetCompleteView):
+    template_name = 'accounts/password_reset_complete.html'

955d924173d8adc886c15ba1f9d39b3b3dc90add
parsa rahbari <53294224+parsa-rahbari-82@users.noreply.github.com>
2020-07-25 23:26:51 +0430

    Update models.py
    some changes for Category model

--- blog/models.py
+++ blog/models.py
@@ -2,16 +2,12 @@ from django.db import models
 
 
 class Category(models.Model):
-    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='sub_category', null=True,
-                                     blank=True)
-    is_sub = models.BooleanField(default=False)
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
 
     class Meta:
         ordering = ('name',)
-        verbose_name_plural = 'categories'
-        verbose_name = 'category'
+        verbose_name_plural = 'Categories'
 
     def __str__(self):
         return self.name

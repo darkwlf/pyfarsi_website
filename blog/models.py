1fd8b0772aceb0de778c6650c915b7ef70225d93
Rez <rezfd99@gmail.com>
2020-07-25 23:50:30 +0430

    small change

--- blog/models.py
+++ blog/models.py
@@ -2,12 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
-    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='sub_category', null=True,
-                                     blank=True)
+    sub_category = models.ForeignKey(
+        'self', on_delete=models.CASCADE, related_name='sub_category', null=True, blank=True
+    )
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
 
     class Meta:
+        db_table = 'pyfarsi_categories'
         ordering = ('name',)
         verbose_name_plural = 'Categories'
 

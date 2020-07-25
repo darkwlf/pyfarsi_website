7b9157501434e0393cce87680cca8ba08e3036cc
parsa rahbari <53294224+parsa-rahbari-82@users.noreply.github.com>
2020-07-25 23:34:41 +0430

    Update models.py
    undo a mistake

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,8 @@ from django.db import models
 
 
 class Category(models.Model):
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='sub_category', null=True,
+                                     blank=True)
     name = models.CharField(max_length=50)
     slug = models.SlugField(max_length=50, unique=True)
 

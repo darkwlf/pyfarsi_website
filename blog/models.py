3c2044ee0321e629594c658551f1492a5e7bd468
parsa rahbari <53294224+parsa-rahbari-82@users.noreply.github.com>
2020-07-25 22:07:14 +0430

    Category model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    sub_category = models.ForeignKey('self', on_delete=models.CASCADE, related_name='sub_category', null=True,
+                                     blank=True)
+    is_sub = models.BooleanField(default=False)
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+        verbose_name_plural = 'categories'
+        verbose_name = 'category'
+
+    def __str__(self):
+        return self.name

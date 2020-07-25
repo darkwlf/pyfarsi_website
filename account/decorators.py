d212a7c66d44e2b451945749944e2ab6981de9a5
Rez <rezfd99@gmail.com>
2020-07-25 16:03:41 +0430

    add decorator for save ip

--- account/decorators.py
+++ account/decorators.py
@@ -1,11 +1,24 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpRequest
 from django.shortcuts import redirect
+from ipware import get_client_ip
 
 
 def not_logged_in(func):
     def check(request: HttpRequest, /, *args, **kwargs):
         if request.user.is_authenticated:
             return redirect('account:profile')
+        return func(request, *args, **kwargs)
+
+    return check
+
+
+@login_required
+def login_required_save_ip(func):
+    def check(request: HttpRequest, /, *args, **kwargs):
+        if request.user.is_authenticated:
+            request.user.ip = get_client_ip(request)[0]
+            request.user.save()
         return func(request, *args, **kwargs)
 
     return check

ad600a9c3a1e263b1c1282d40a1889f1959f0bb3
parsa rahbari <53294224+parsa-rahbari-82@users.noreply.github.com>
2020-07-26 17:22:57 +0430

    search view
    added search view

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.db.models import Q
+from .models import Article, Category
 
-# Create your views here.
+class SerachView(ListView):
+    model = City
+    template_name = 'serach_result.html'
+
+    def get_queryset(self):
+        query = self.request.GET.get('q', False)
+        if query:
+
+          article_list = Article.objects.filter(
+              Q(title__icontains=query) | Q(content__icontains=query)
+          )
+          category_list = Category.objects.filter(
+              Q(name__icontains=query)
+          )
+        else:
+           article_query = Article.objects.all()
+           category_query = Category.objects.all()
+           object_list = article_query + category_query
+
+        return object_list

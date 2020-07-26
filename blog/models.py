67fdd8c53896d75d8f805a00be497a9b5f464123
Erfan2003 <Erfan20032003@gmail.com>
2020-07-26 07:57:26 +0000

    slug and status fields added

--- blog/models.py
+++ blog/models.py
@@ -3,12 +3,18 @@ from django.conf import settings
 
 
 class Article(models.Model):
+    class Status(models.TextChoices):
+        DRAFT = "d"
+        PUBLISH = "p"
+
     title = models.CharField(max_length=220, verbose_name="Title")
     content = models.TextField(verbose_name="Content")
     author = models.ForeignKey(
         settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name="articles"
     )
     date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=1, choices=Status.choices)
+    slug = models.SlugField(max_length=1024, verbose_name="Slug")
 
     class Meta:
         db_table = "pyfarsi_articles"

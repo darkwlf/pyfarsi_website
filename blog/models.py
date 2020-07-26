ffd691eb0839854a7abc379ef65f56a6a993028e
Erfan2003 <Erfan20032003@gmail.com>
2020-07-26 07:24:11 +0000

    Article model added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=220, verbose_name="Title")
+    content = models.TextField(verbose_name="Content")
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name="articles"
+    )

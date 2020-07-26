2a711ab03f8b8fd4e98abc7cf430e41246a54e7c
Rez <rezfd99@gmail.com>
2020-07-26 20:21:28 +0430

    small change

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,22 @@
 from django.shortcuts import render
 from django.db.models import Q
 from .models import Article
-from django.views.generic import ListView 
+from django.views.generic import ListView
 
 
 class Article(ListView):
     model = Article
     template_name = 'article.html'
-    context_object_name = 'articles'
-    paginate_by = 10
+    paginate_by = 9
+
     def get_queryset(self):
         query = self.request.GET.get('q')
         if query:
-          article_query = Article.objects.filter(
-              Q(title__icontains=query) | Q(content__icontains=query)
-          )     
-                        
+            article_query = Article.objects.filter(
+                Q(title__icontains=query) | Q(content__icontains=query)
+            )
+
         else:
-           article_query = Article.objects.all()
+            article_query = Article.objects.all()
 
         return article_query

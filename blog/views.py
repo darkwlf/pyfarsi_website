262de2710048bd1d75ed5e04ee483967614a2a18
parsa rahbari <53294224+parsa-rahbari-82@users.noreply.github.com>
2020-07-26 18:16:35 +0430

    Update views.py
    some bugs fixed for

--- blog/views.py
+++ blog/views.py
@@ -1,27 +1,21 @@
 from django.shortcuts import render
 from django.db.models import Q
-from .models import Article, Category
+from .models import Article
+django.views.generic import listview
 
-class SerachView(ListView):
-    model = City
-    template_name = 'serach_result.html'
+
+class ArticleView(ListView):
+    model = Article
+    template_name = 'article.html'
 
     def get_queryset(self):
-        query = self.request.GET.get('q', False)
+        query = self.request.GET.get('q')
         if query:
-
-          article_list = Article.objects.filter(
+          article_query = Article.objects.filter(
               Q(title__icontains=query) | Q(content__icontains=query)
-          )
-          category_list = Category.objects.filter(
-              Q(name__icontains=query)
-          )
-                     
-          object_list = article_list + category_list           
-
+          )     
+                        
         else:
            article_query = Article.objects.all()
-           category_query = Category.objects.all()
-           object_list = article_query + category_query
 
-        return object_list
+        return article_query

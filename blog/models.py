f04dc00e070ab1a7e165094a8f5b76ae37641c81
NitroZeus <realantiwanted@gmail.com>
2020-07-26 17:36:22 +0430

    Add Comment model and edit Article model

--- blog/models.py
+++ blog/models.py
@@ -1,31 +1,36 @@
 from django.db import models
 from django.conf import settings
 from ckeditor_uploader.fields import RichTextUploadingField
+from django.shortcuts import reverse
 
 
 class Article(models.Model):
     class Status(models.TextChoices):
-        DRAFT = "d"
-        PUBLISH = "p"
+        DRAFT = 'd', 'Draft'
+        PUBLISHED = 'p', 'Published'
     
-    title = models.CharField(max_length=220, verbose_name="Title")
+    title = models.CharField(max_length=220)
     content = RichTextUploadingField()
-    author = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name="articles"
-    )
-    date = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=1, choices=Status.choices, verbose_name="Status")
-    slug = models.SlugField(max_length=50, verbose_name="Slug")
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, related_name='article_author')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='Creation Date')
+    status = models.CharField(max_length=1, choices=Status.choices)
+    slug = models.SlugField(max_length=50)
+
+    def get_absolute_url(self):
+        return reverse('blog:articles', self.id, 1, self.slug)
 
     class Meta:
-        db_table = "pyfarsi_articles"
+        db_table = 'pyfarsi_articles'
+
+    def __str__(self):
+        return f'{self.id} : {self.author.username}'
 
         
 class Category(models.Model):
     sub_category = models.ForeignKey(
         'Category', on_delete=models.CASCADE, related_name='sub_category', null=True, blank=True
     )
-    name = models.CharField(max_length=40)
+    name = models.CharField(max_length=40, primary_key=True)
     slug = models.SlugField(max_length=50, unique=True)
 
     class Meta:
@@ -34,4 +39,23 @@ class Category(models.Model):
         verbose_name_plural = 'Categories'
 
     def __str__(self):
-        return f'{self.name}:{self.sub_category.name}'
+        return f'{self.name} : {self.sub_category.name}'
+
+
+class Comment(models.Model):
+    class Status(models.TextChoices):
+        PENDING = 'w', 'Pending'
+        PUBLISHED = 'p', 'Published'
+
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, models.CASCADE, 'comment_author')
+    content = models.TextField()
+    date = models.DateTimeField('Creation Date', auto_now_add=True)
+    article = models.ForeignKey(Article, models.CASCADE, 'comment_article')
+    status = models.CharField(max_length=1, choices=Status.choices, default=Status.PENDING)
+
+    class Meta:
+        db_table = 'pyfarsi_comments'
+        ordering = ('id',)
+
+    def __str__(self):
+        return f'{self.author.username} : {self.article.title}'

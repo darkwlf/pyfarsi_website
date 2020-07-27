8c18efe84488d6903c6699cba51f8bd4a7df9040
Rez <rezfd99@gmail.com>
2020-07-27 17:34:23 +0430

    add generator for get all sub category and add generators file in utils package

---
+++ utils/generators.py
@@ -0,0 +1,10 @@
+from blog.models import Category
+
+
+def get_sub_category(category: Category):
+    if (subs := category.sub_categories) is None:
+        yield category
+    else:
+        for sub in subs:
+            yield sub
+            get_sub_category(sub)

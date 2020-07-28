4b993c6fe697188b0c1ee6048f8cb13718305b03
Rez <rezfd99@gmail.com>
2020-07-28 10:18:59 +0430

    fix bugs to register view in account

--- account/views.py
+++ account/views.py
@@ -44,28 +44,32 @@ class PasswordResetComplete(auth_views.PasswordResetCompleteView):
 
 class RegisterView(NotLoggedIn, CreateView):
     form_class = Register
+    template_name = 'account/register.html'
     success_url = 'account:register_complete'
 
     def form_valid(self, form):
         temp_user = form.save(False)
         temp_user.set_password(temp_user.password)
-        instance = temp_user.save()
+        temp_user.save()
+        from django.urls import reverse
         email_template = render_to_string(
             'account/email_validation.html',
             {
-                'user': instance,
-                'validation': Validation.objects.create(user=instance).key,
-                'base_domain': f'https://{settings.ALLOWED_HOSTS[0]}/'
+                'user': temp_user,
+                'validation_url': reverse(
+                    'account:verify_email', kwargs={'key': Validation.objects.create(user=temp_user).key}
+                ),
+                'base_domain': f'https://{settings.ALLOWED_HOSTS[0]}'
             }
         )
         send_mail(
             'فعال سازی حساب',
             strip_tags(email_template),
             settings.EMAIL_HOST_USER,
-            (instance.email,),
+            (temp_user.email,),
             html_message=email_template
         )
-        remove_user(instance.username)
+        remove_user(temp_user.username)
         return redirect(self.success_url)
 
 

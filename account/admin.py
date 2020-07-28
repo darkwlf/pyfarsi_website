c8feeafb2d3e500a16c5704e94a5bab0dc5b5b9f
Rez <rezfd99@gmail.com>
2020-07-28 17:50:07 +0430

    add model admin for validation

--- account/admin.py
+++ account/admin.py
@@ -7,7 +7,7 @@ class User(admin.ModelAdmin):
     list_display = ('username', 'email', 'date_joined', 'is_staff', 'is_active', 'last_login')
     list_per_page = 15
     search_fields = ('username', 'email', 'first_name', 'last_name')
-    readonly_fields = ('date_joined', 'last_login')
+    readonly_fields = ('date_joined', 'last_login', 'password')
     date_hierarchy = 'date_joined'
     filter_horizontal = ('user_permissions', 'groups')
     fieldsets = (
@@ -15,3 +15,12 @@ class User(admin.ModelAdmin):
         ('Status', {'fields': ('is_active', 'is_staff', 'last_login', 'is_superuser', 'ip')}),
         ('Permissions', {'fields': ('user_permissions', 'groups')})
     )
+
+
+@admin.register(models.Validation)
+class Validation(admin.ModelAdmin):
+    list_display = ('key', 'user')
+    list_per_page = 15
+    search_fields = ('user__username', 'user__email', 'user__first_name', 'user__last_name')
+    readonly_fields = ('key',)
+    fieldsets = (('Information', {'fields': ('key', 'user')}),)

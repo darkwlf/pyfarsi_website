559d8ef1c62347af5e1d7dc2c8f74bb612c35823
NitroZeus <realantiwanted@gmail.com>
2020-07-28 21:00:00 +0430

    Fix translation bugs

--- account/models.py
+++ account/models.py
@@ -12,15 +12,16 @@ class User(AbstractUser):
         self.__password = self.password
 
     email = models.EmailField(verbose_name=translations.email, unique=True)
-    is_active = models.BooleanField(verbose_name=_('Is Active'), default=False)
+    is_active = models.BooleanField(verbose_name=_('active'), default=False)
     first_name = models.CharField(max_length=30, verbose_name=translations.first_name)
     last_name = models.CharField(max_length=30, verbose_name=translations.last_name)
     phone_number = PhoneNumberField(verbose_name=translations.phone_number)
-    password = models.CharField(verbose_name='Password', help_text=_('Encrypted Password'), max_length=128)
-    ip = models.GenericIPAddressField(verbose_name=_('Last IP Address'), null=True, blank=True)
+    ip = models.GenericIPAddressField(verbose_name=_('IP address'), null=True, blank=True)
     REQUIRED_FIELDS = ('first_name', 'last_name', 'email', 'phone_number')
 
     class Meta:
+        verbose_name = translations.user
+        verbose_name_plural = _('users')
         db_table = 'pyfarsi_users'
         ordering = ('-date_joined',)
 
@@ -34,9 +35,10 @@ class User(AbstractUser):
 
 
 class Validation(models.Model):
-    key = models.UUIDField(default=uuid4, verbose_name=_('Validation Key'), primary_key=True)
-    user = models.ForeignKey(User, models.CASCADE, 'validations_user', verbose_name=_('User'))
+    key = models.UUIDField(default=uuid4, verbose_name=_('validation key'), primary_key=True)
+    user = models.ForeignKey(User, models.CASCADE, 'validations_user', verbose_name=translations.user)
 
     class Meta:
+        verbose_name = _('validation')
         db_table = 'pyfarsi_validations'
         ordering = ('-user__date_joined',)

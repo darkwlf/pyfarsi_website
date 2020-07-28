d7737b45f054011c8f93a98915095e57219ca6f1
parsa rahbari <53294224+parsa-rahbari-82@users.noreply.github.com>
2020-07-28 13:05:17 +0430

    Update forms.py
    Create a Profile ModelForm for Profile view

--- account/forms.py
+++ account/forms.py
@@ -37,3 +37,26 @@ class Register(forms.ModelForm):
         if self.cleaned_data.get('password') and self.cleaned_data.get('password_confirm') and \
                 self.cleaned_data['password'] != self.cleaned_data.pop('password_confirm'):
             self.add_error('password_confirm', 'پسورد های وارد شده یکسان نیستند !')
+
+class Profile(forms.ModelForm):
+    model = User
+    exclude = ('date_joined', 'is_active', 'is_superuser', 'is_staff', 'password')
+    error_messages = {
+            'username': {'unique': 'این نام کاربری قبلا ثبت شده است !'},
+            'email': {'unique': 'این ایمیل قبلا توسط فرد دیگری استفاده شده است .'}
+    }
+    labels = {
+            'username': 'یوزرنیم',
+            'email': 'ایمیل',
+            'first_name': 'نام',
+            'last_name': 'نام خانوادگی',
+            'phone_number': 'شماره تلفن'
+    }
+    widgets = {
+            'username': forms.TextInput({'placeholder': 'نام کاربری'}),
+            'email': forms.TextInput({'placeholder': 'پست الکترونیکی'}),
+            'first_name': forms.TextInput({'placeholder': 'نام'}),
+            'last_name': forms.TextInput({'placeholder': 'نام خانوادگی'}),
+            'phone_number': PhoneNumberPrefixWidget({'placeholder': 'شماره تلفن'})
+        }
+

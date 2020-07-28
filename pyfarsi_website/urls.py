67d83ffcf74808112aec76cdfc38693788466a26
Rez <rezfd99@gmail.com>
2020-07-28 17:44:17 +0430

    add url ck-editor and blog to base urls

--- pyfarsi_website/urls.py
+++ pyfarsi_website/urls.py
@@ -5,5 +5,7 @@ from .views import index
 urlpatterns = (
     path("admin/", admin.site.urls),
     path("", index, name="index"),
-    path('account/', include('account.urls'))
+    path('account/', include('account.urls')),
+    path('blog/', include('blog.urls')),
+    path('ck-editor/', include('ckeditor_uploader.urls'))
 )

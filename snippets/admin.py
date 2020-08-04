623961882f144bec907feaf293460aad13a47eff
RezFD <rezfd99@gmail.com>
2020-08-04 17:37:01 +0430

    add admin for Group snippets

--- snippets/admin.py
+++ snippets/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Group)
+class Group(admin.ModelAdmin):
+    list_display = ('title', 'type', 'creation_date')
+    search_fields = ('title',)
+    readonly_fields = ('creation_date',)
+    date_hierarchy = 'creation_date'
+    list_per_page = 15
+    fieldsets = (
+        ('Information', {'fields': ('logo', ('title', 'slug'), 'description')}),
+        ('Status', {'fields': ('type', 'creation_date')})
+    )

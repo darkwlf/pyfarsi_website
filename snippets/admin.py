965b9b38a6c5ac0d3ea7fba67ec96225e0c5edd0
RezFD <rezfd99@gmail.com>
2020-08-04 17:50:48 +0430

    small edit

--- snippets/admin.py
+++ snippets/admin.py
@@ -4,8 +4,8 @@ from . import models
 
 @admin.register(models.Group)
 class Group(admin.ModelAdmin):
-    list_display = ('title', 'type', 'creation_date')
-    search_fields = ('title',)
+    list_display = ('name', 'type', 'creation_date')
+    search_fields = ('name',)
     readonly_fields = ('creation_date',)
     date_hierarchy = 'creation_date'
     list_per_page = 15

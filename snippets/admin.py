34f5e28bafd5a3e2a0cacfe458f34c78e1e43289
RezFD <rezfd99@gmail.com>
2020-08-05 20:42:48 +0430

    add InviteLinl and UserLink in admin

--- snippets/admin.py
+++ snippets/admin.py
@@ -29,3 +29,30 @@ class Member(admin.ModelAdmin):
         ('Information', {'fields': ('id', 'group', 'user')}),
         ('Status', {'fields': ('date_joined',)})
     )
+
+
+@admin.register(models.InviteLink)
+class InviteLink(admin.ModelAdmin):
+    list_display = ('invite_id', 'group', 'status', 'users_joined')
+    search_fields = ('group__name',)
+    list_filter = ('status', 'group__type')
+    readonly_fields = ('invite_id',)
+    list_per_page = 15
+    fieldsets = (
+        ('Information', {'fields': ('invite_id', 'group', 'users_joind')}),
+        ('Status', {'fields': ('status',)})
+    )
+
+
+@admin.register(models.UserInvite)
+class UserInvite(admin.ModelAdmin):
+    list_display = ('user', 'group', 'status', 'creation_date')
+    search_fields = ('group__name', 'user__username', 'user__email', 'user__first_name', 'user__last_name')
+    list_filter = ('status', 'group__type', 'user__is_staff')
+    readonly_fields = ('creation_date',)
+    date_hierarchy = 'creation_date'
+    list_per_page = 15
+    fieldsets = (
+        ('Information', {'fields': (('user', 'group'),)}),
+        ('Status', {'fields': ('status', 'creation_date')})
+    )

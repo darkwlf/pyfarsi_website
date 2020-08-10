5b3cf80ec9a44ddaccc81e0625d6f1bf399e6615
Dark Wolf <63583981+darkwlf@users.noreply.github.com>
2020-08-10 12:31:35 +0430

    Update admin.py

--- snippets/admin.py
+++ snippets/admin.py
@@ -88,3 +88,16 @@ class TelegramGroup(admin.ModelAdmin):
     fieldsets = (
         ('Information', {'fields': (('name', 'slug'), 'code', ('group', 'user'))}),)
     )
+
+    
+@admin.register(models.ScreenShot)
+class ScreenShot(admin.ModelAdmin):
+    list_display = ('id', 'name', 'snippet', 'creation_date')
+    search_fields = ('name', *USER_SEARCH_FILTER)
+    list_filter = ('status', 'group__type', 'user__is_staff')
+    readonly_fields = ('creation_date', 'id')
+    date_hierarchy = 'creation_date'
+    list_per_page = 15
+    fieldsets = (
+        ('Information', {'fields': (('name', 'slug', 'lang'), 'code', ('group', 'user'))}),)
+    )
